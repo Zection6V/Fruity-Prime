a9cbdb1f680c5fe2e71e11cf9c9073f1375bbbac
Zection6V <zection6vmph@gmail.com>
2026-09-11 12:38:19 +0900

    Port Enemy51Entity CarnivorousPlant to C++20

---
+++ src/MphRead.Native/Entities/Enemies/51_CarnivorousPlant.cpp
@@ -0,0 +1,46 @@
+#include "51_CarnivorousPlant.hpp"
+
+#include "../EnemySpawnEntity.hpp"
+
+#include <cassert>
+
+namespace MphRead::Entities
+{
+    namespace
+    {
+        EnemySpawnEntity* CastSpawner(EntityBase* spawner) noexcept
+        {
+            EnemySpawnEntity* typedSpawner = dynamic_cast<EnemySpawnEntity*>(spawner);
+            assert(typedSpawner != nullptr);
+            return typedSpawner;
+        }
+    }
+
+    Enemy51Entity::Enemy51Entity(EnemyInstanceEntityData data,
+        Formats::Culling::NodeRef nodeRef, Scene* scene)
+        : EnemyInstanceEntity(data, nodeRef, scene),
+          _spawner(CastSpawner(data.Spawner))
+    {
+    }
+
+    void Enemy51Entity::EnemyInitialize()
+    {
+        Transform = _data.Spawner->Transform;
+        _prevPos = Position;
+        Flags |= EnemyFlags::Visible;
+        Flags |= EnemyFlags::Static;
+        Flags |= EnemyFlags::NoMaxDistance; // todo: game doesn't set this
+        _health = _healthMax = _spawner->Data.Fields.S07.EnemyHealth;
+        _boundingRadius = Fixed::ToFloat(1843);
+        _hurtVolumeInit = CollisionVolume(
+            ::System::Numerics::Vector3(0.0F, Fixed::ToFloat(409), 0.0F), _boundingRadius);
+        _hurtVolume = CollisionVolume::Transform(_hurtVolumeInit, Transform);
+        ObjectMetadata meta = Metadata::GetObjectById(_spawner->Data.Fields.S07.EnemySubtype);
+        SetUpModel(meta.Name);
+    }
+
+    void Enemy51Entity::EnemyProcess()
+    {
+        ContactDamagePlayer(_spawner->Data.Fields.S07.EnemyDamage, false);
+    }
+}
---
+++ src/MphRead.Native/Entities/Enemies/51_CarnivorousPlant.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../../Formats/Culling.hpp"
+#include "../EnemyInstanceEntity.hpp"
+
+namespace MphRead::Entities
+{
+    class EnemySpawnEntity;
+
+    class Enemy51Entity : public EnemyInstanceEntity
+    {
+    public:
+        Enemy51Entity(EnemyInstanceEntityData data,
+            Formats::Culling::NodeRef nodeRef, Scene* scene);
+
+        Enemy51Entity(const Enemy51Entity&) = delete;
+        Enemy51Entity& operator=(const Enemy51Entity&) = delete;
+        Enemy51Entity(Enemy51Entity&&) = delete;
+        Enemy51Entity& operator=(Enemy51Entity&&) = delete;
+
+    protected:
+        void EnemyInitialize() override;
+        void EnemyProcess() override;
+
+    private:
+        EnemySpawnEntity* const _spawner;
+    };
+}

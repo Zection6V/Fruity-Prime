460188df3c2473f7ebc50a33c5b670dba2e2b2b6
Zection6V <zection6vmph@gmail.com>
2026-09-11 11:04:22 +0900

    Port HomeWindow to native C++20

---
+++ src/MphRead.Native/Mods/Launcher/Gui/HomeWindow.cpp
@@ -0,0 +1,36 @@
+#include "HomeWindow.hpp"
+
+#include "../../Branding.hpp"
+
+namespace MphRead::Mods::Launcher::Gui
+{
+    HomeWindow::HomeWindow(HomeWindowAdapter& adapter, HomeWindowMenuSettingsRef settings,
+        HomeWindowRoomsRef rooms)
+        : _adapter(adapter),
+          _view(_adapter.ConstructHomeView(settings, rooms))
+    {
+        _adapter.ConnectHomeViewDone(_view, this, &HomeWindow::OnDone);
+
+        _adapter.SetTitle(MphRead::Mods::Branding::Name);
+        _adapter.SetIcon(HomeWindowIcon::GuiThemeAppIconValue);
+        _adapter.SetWidth(940.0);
+        _adapter.SetHeight(560.0);
+        _adapter.SetMinWidth(780.0);
+        _adapter.SetMinHeight(480.0);
+        _adapter.SetWindowStartupLocation(HomeWindowStartupLocation::CenterScreen);
+        _adapter.SetBackground(HomeWindowBrush::GuiThemePanelBrush);
+        _adapter.SetRequestedThemeVariant(HomeWindowThemeVariant::Dark);
+        _adapter.SetContent(_view);
+    }
+
+    LaunchPlan HomeWindow::Plan() const
+    {
+        return _adapter.GetHomeViewPlan(_view);
+    }
+
+    void HomeWindow::OnDone(void* context)
+    {
+        auto& window = *static_cast<HomeWindow*>(context);
+        window._adapter.Close();
+    }
+}
---
+++ src/MphRead.Native/Mods/Launcher/Gui/HomeWindow.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "../Portable/LaunchPlan.hpp"
+
+#include <cstdint>
+#include <string_view>
+
+namespace MphRead::Mods::Launcher::Gui
+{
+    struct HomeWindowMenuSettingsRef
+    {
+        void* Native;
+    };
+
+    struct HomeWindowRoomsRef
+    {
+        void* Native;
+    };
+
+    enum class HomeWindowStartupLocation : std::uint8_t
+    {
+        CenterScreen
+    };
+
+    enum class HomeWindowIcon : std::uint8_t
+    {
+        GuiThemeAppIconValue
+    };
+
+    enum class HomeWindowBrush : std::uint8_t
+    {
+        GuiThemePanelBrush
+    };
+
+    enum class HomeWindowThemeVariant : std::uint8_t
+    {
+        Dark
+    };
+
+    class HomeWindowAdapter
+    {
+    public:
+        using HomeViewHandle = void*;
+        using DoneHandler = void (*)(void* context);
+
+        virtual ~HomeWindowAdapter() = default;
+
+        virtual HomeViewHandle ConstructHomeView(HomeWindowMenuSettingsRef settings,
+            HomeWindowRoomsRef rooms) = 0;
+        virtual void ConnectHomeViewDone(HomeViewHandle view, void* context,
+            DoneHandler handler) = 0;
+        [[nodiscard]] virtual LaunchPlan GetHomeViewPlan(HomeViewHandle view) const = 0;
+
+        virtual void Close() = 0;
+        virtual void SetTitle(std::string_view title) = 0;
+        virtual void SetIcon(HomeWindowIcon icon) = 0;
+        virtual void SetWidth(double width) = 0;
+        virtual void SetHeight(double height) = 0;
+        virtual void SetMinWidth(double minWidth) = 0;
+        virtual void SetMinHeight(double minHeight) = 0;
+        virtual void SetWindowStartupLocation(HomeWindowStartupLocation location) = 0;
+        virtual void SetBackground(HomeWindowBrush brush) = 0;
+        virtual void SetRequestedThemeVariant(HomeWindowThemeVariant variant) = 0;
+        virtual void SetContent(HomeViewHandle content) = 0;
+    };
+
+    class HomeWindow final
+    {
+    public:
+        HomeWindow(HomeWindowAdapter& adapter, HomeWindowMenuSettingsRef settings,
+            HomeWindowRoomsRef rooms);
+
+        [[nodiscard]] LaunchPlan Plan() const;
+
+    private:
+        static void OnDone(void* context);
+
+        HomeWindowAdapter& _adapter;
+        HomeWindowAdapter::HomeViewHandle _view;
+    };
+}

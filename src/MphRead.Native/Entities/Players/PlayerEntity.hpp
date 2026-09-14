4ddd8fb69a6c0eb7a03b37ea4b41ecc84a1e2b7c
Zection6V <zection6vmph@gmail.com>
2026-09-14 21:11:33 +0900

    WIP PlayerEntity native parity

---
+++ src/MphRead.Native/Entities/Players/PlayerEntity.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "DynamicLightEntity.hpp"
+#include "PlayerCamera.hpp"
+#include "PlayerCollision.hpp"
+#include "PlayerDialog.hpp"
+#include "PlayerDraw.hpp"
+#include "../EnemyInstanceEntity.hpp"
+#include "../../Formats/Culling.hpp"
+#include "../../Formats/Formats.hpp"
+#include "../../Formats/Model.hpp"
+#include "../../Formats/Types.hpp"

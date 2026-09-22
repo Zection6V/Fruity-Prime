97792895db92b45531ae7196c418449fad92eeb7
Zection6V <zection6vmph@gmail.com>
2026-09-22 11:12:46 +0900

    Fix MapPacker list exception identity parity

--- src/MphRead.Native/Mods/MapGen/MapPacker.cpp
+++ src/MphRead.Native/Mods/MapGen/MapPacker.cpp
@@ -207,8 +207,7 @@ namespace
 
     [[noreturn]] void ListBounds()
     {
-        throw std::out_of_range(
-            "Index was out of range. Must be non-negative and less than the size of the collection. (Parameter 'index')");
+        throw System::ArgumentOutOfRangeException();
     }
 
     [[noreturn]] void ArrayBounds()

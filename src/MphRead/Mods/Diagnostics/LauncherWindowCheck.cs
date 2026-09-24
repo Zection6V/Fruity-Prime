85a49c39bef97ff74c1be1af53284cd0975ff1ac
Zection6V <zection6vmph@gmail.com>
2026-09-24 10:02:13 +0900

    ci: validate Vulkan through the render facade

--- src/MphRead/Mods/Diagnostics/LauncherWindowCheck.cs
+++ src/MphRead/Mods/Diagnostics/LauncherWindowCheck.cs
@@ -3,6 +3,7 @@ using System;
 using MphRead.Mods.Launcher.Gui;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
+using GL = MphRead.Mods.Render.RenderGl;
 
 namespace MphRead.Mods.Diagnostics
 {
